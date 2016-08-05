af6f8ebdfa507801bbf0634a3ee18f8eeac7ce85
Denis Luchkin-Zhou <denis@ricepo.com>
2016-08-05 05:38:44 -0700

    More work on isodistance

--- src/index.js
+++ src/index.js
@@ -9,123 +9,141 @@
 /* eslint no-loop-func: 1 */
 const _            = require('lodash');
 const FS           = require('fs');
-const Ora          = require('ora');
+const OSRM         = require('osrm');
 const Turf         = require('turf');
-const MapBox       = require('mapbox');
 const Bluebird     = require('bluebird');
 const GeoPoint     = require('geopoint');
-const Dissolve     = require('turf-dissolve');
 
 const Merge        = require('turf-merge');
 const Line2poly    = require('turf-line-to-polygon');
 
 
-const mbox = new MapBox(
-  'pk.eyJ1Ijoid3l2ZXJuem9yYSIsImEiOiJjaXJoMWVjeWQwMTZoZ2RreDFlYjV5aTJ2In0.CzAKGTcFvqk_08InM1fSnw'
-);
-
-/**
- * Determine the bounding box of the point-grid
- */
-
-
-function l(v) {
-  console.log(JSON.stringify(v, null, 1));
-}
-
-function p(v) {
-  return { latitude: v.coordinates[1], longitude: v.coordinates[0] };
-}
+const osrm = new OSRM('osrm/indiana-latest.osrm');
+Bluebird.promisifyAll(osrm);
+Bluebird.promisifyAll(FS);
 
 
 const origin = {
   type: 'Point',
-  coordinates: [ -77.594544, 43.138428 ]
+  coordinates: [ -86.893386, 40.417202 ]
 };
 
-const thresholds = [ 3, 7 ];
+const thresholds = [ 1, 5, 9 ];
 
-const gp = new GeoPoint(origin.coordinates[1], origin.coordinates[0]);
-const bb = gp.boundingCoordinates(_.max(thresholds));
 
-const box = Turf.bbox(
-  Turf.featureCollection(
-    bb.map(
-      i => Turf.point([ i._degLon, i._degLat ])
-    )
-  )
-);
-
+function log(data) {
+  process.stdout.write(_.padEnd(`\r${data}`, process.stdout.columns));
+}
 
 
 (async function() {
-  const spinner = Ora('Starting up...');
-  spinner.start();
+  process.stdout.write('Loading...');
+
+
+
+  /**
+   * Determine the bounding box and generate point grid
+   */
+  const box = Turf.bbox(
+    Turf.featureCollection(
+      new GeoPoint(origin.coordinates[1], origin.coordinates[0])
+        .boundingCoordinates(_.max(thresholds))
+        .map(i => Turf.point([ i._degLon, i._degLat ]))
+    )
+  );
 
 
 
   /**
    * Generate the point grid
    */
-  const pgrid = Turf.pointGrid(box, 0.5, 'miles');
+  const pgrid = Turf.pointGrid(box, 0.05, 'miles');
 
 
 
   /**
    * Compute distances
    */
-  const chunks = _.chunk(pgrid.features, 60);
-
-  const tf = pgrid.features.length;
-  let   cf = 0;
+  const chunks = _.chunk(pgrid.features, 1000);
 
   const tc = chunks.length;
   let   cc = 0;
 
-
   for (const chunk of chunks) {
-
-    const promises = chunk.map(async feature => {
-      const pt = feature.geometry;
-      const dt = (await mbox.getDirections([ p(origin), p(pt) ])).routes[0].distance * 0.000621371;
-      spinner.text = `Fetched ${cf++} (${cc} chunks) of ${tf} (${tc} chunks)...`;
-      feature.properties.z = dt;
-      feature.properties.a = 'a';
-
-      return feature;
-    });
+    const promises = chunk.map(computeDistance);
 
     await Bluebird.all(promises);
-    // await Bluebird.delay(1000);
-    cc++;
-
+    log(`Computing distances: ${(cc++ / tc * 100).toFixed(2)}%`);
   }
 
-  const features = pgrid;
+
+  /**
+   * Generate isolines and convert them to polygons
+   */
+  log('Computing isolines, this may take a while...');
+  const isolines = Turf.isolines(pgrid, 'z', 25, thresholds);
+
+  log('Fitting to hex grid...');
+  isolines.features = isolines
+    .features
+    .map(Line2poly)
+    .map(hexfit);
 
 
-  const isolines = Turf.isolines(features, 'z', 25, thresholds);
 
-  isolines.features = isolines.features.map(i => hexfit(Line2poly(i)));
+  log('Writing result to file...');
+  await FS.writeFileAsync('result.json', JSON.stringify(isolines, null, 2), 'utf8');
 
-  spinner.text = 'Fitting to hex grid...';
+  log('Success!');
+
+  /**
+   * Compute distance of a point from the origin
+   */
+  async function computeDistance(feature) {
+    const pt = feature.geometry;
+
+    const routes = await osrm.routeAsync({
+      coordinates: [ origin.coordinates, pt.coordinates ]
+    });
+
+    feature.properties.z = routes.routes[0].distance * 0.000621371;
+  }
 
 
+  /**
+   * Dissolve individual polygons
+   */
 
+  /**
+   * Fit the polygon over a hex grid
+   */
+  function hexfit(feature) {
+    const z = feature.properties.z;
 
-  spinner.stop();
+    const grid = Turf.hexGrid(box, 0.5, 'miles').features;
+    let c = 0;
+    const hex = _
+      .chain(grid)
+      .filter(i => {
+        log(`Fitting z=${z}: ${(c++ / grid.length * 100).toFixed(2)}%`);
+        return Turf.intersect(i, feature);
+      })
+      .map(round)
+      .value();
 
-  FS.writeFileSync('result.json', JSON.stringify(isolines, null, 2), 'utf8');
+    log(`Fitting z=${z}: merging, this may take a while...`);
+    return Merge(Turf.featureCollection(hex));
+  }
+
+  function round(feature) {
+    const coords = feature.geometry.coordinates[0];
+    feature.geometry.coordinates[0] = coords.map(i => [ r(i[0], 6), r(i[1], 6) ]);
+    return feature;
+  }
+
+  function r(num, decimals) {
+    const c = Math.pow(10, decimals);
+    return Math.round(num * c) / c;
+  }
 
 }());
-
-
-function hexfit(feature) {
-
-  const hex = _
-    .chain(Turf.hexGrid(box, 0.5, 'miles').features)
-    .filter(i => Turf.intersect(i, feature))
-    .value();
-  return Dissolve(Merge(Turf.featureCollection(hex)), 'a');
-
-}
