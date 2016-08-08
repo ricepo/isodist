13833c828e0941add4c4efe07a4e549ff05621f7
Denis Luchkin-Zhou <denis@ricepo.com>
2016-08-08 02:14:19 -0700

    Optional global installation of OSRM, also now working with pbf files

--- script/prepare.sh
+++ script/prepare.sh
@@ -6,6 +6,24 @@ cd osrm
 
 
 #
+# Determine the osrm-extract path
+#
+OSRM_EXTRACT="../.osrm/bin/osrm-extract"
+if type "osrm-extract" > /dev/null; then
+  OSRM_EXTRACT=$(which "osrm-extract")
+fi
+
+
+#
+# Determine the osrm-contract path
+#
+OSRM_CONTRACT="../.osrm/bin/osrm-contract"
+if type "osrm-contract" > /dev/null; then
+  OSRM_CONTRACT=$(which "osrm-contract")
+fi
+
+
+#
 # Link the car profile to current directory
 #
 if [ ! -f profile.lua ]; then
@@ -16,15 +34,15 @@ fi
 #
 # Extract and contract each OSM file
 #
-for f in *.osm; do
-  base=$(basename "$f" .osm)
+for f in *.osm.pbf; do
+  base=$(basename "$f" .osm.pbf)
 
   if [ -f "$base.osrm" ]; then
     echo "$base.osrm already exists, skipping..."
     continue
   fi
 
-  ../.osrm/bin/osrm-extract "$base.osm"
-  ../.osrm/bin/osrm-contract "$base.osrm"
+  $OSRM_EXTRACT "$base.osm.pbf"
+  $OSRM_CONTRACT "$base.osrm"
 
 done
